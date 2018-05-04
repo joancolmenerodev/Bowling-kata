702ed652f0a7d68b780bc0fbb806d5f828ab5fe0
joancolmenerodev <joancolmenero@gmail.com>
2018-05-04 12:02:52 +0200

    Slide 49

--- src/main/java/com/joancolmenerodev/bownlingkata/Game.java
+++ src/main/java/com/joancolmenerodev/bownlingkata/Game.java
@@ -4,7 +4,6 @@ public class Game {
     private int rolls[] = new int[21];
     private int currentRoll = 0;
 
-
     public void roll(int pins) {
         rolls[currentRoll++] = pins;
     }
@@ -15,29 +14,33 @@ public class Game {
         for (var frame = 0; frame < 10; frame++) {
             if (rolls[frameIndex] == 10) // strike
             {
-                score += 10 +
-                        rolls[frameIndex+1] +
-                        rolls[frameIndex+2];
+                score += 10 + strikeBonus(frameIndex);
                 frameIndex++;
-            }
-            else if (isSpare(frameIndex))
-            {
-                score += 10 + rolls[frameIndex + 2];
+            } else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                score += rolls[frameIndex] +
-                        rolls[frameIndex + 1];
+                score += sumOfBallsInFrame(frameIndex);
                 frameIndex += 2;
             }
         }
-
         return score;
     }
 
-    private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex] +
-                rolls[frameIndex + 1] == 10;
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex]+rolls[frameIndex+1];
     }
 
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex+1]+rolls[frameIndex+2];
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex]+rolls[frameIndex+1] == 10;
+    }
 
 }
