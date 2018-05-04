69dea9c868259e2d7418f5c0ef490afbe2ca8be1
joancolmenerodev <joancolmenero@gmail.com>
2018-05-04 11:57:11 +0200

    Slide 44

--- src/main/java/com/joancolmenerodev/bownlingkata/Game.java
+++ src/main/java/com/joancolmenerodev/bownlingkata/Game.java
@@ -11,20 +11,21 @@ public class Game {
 
     public int score() {
         var score = 0;
-        var i = 0;
+        var frameIndex = 0;
         for (var frame = 0; frame < 10; frame++) {
-            if (rolls[i] + rolls[i + 1] == 10) // spare
+            if (rolls[frameIndex] +
+                    rolls[frameIndex + 1] == 10) // spare
             {
-                score += 10 + rolls[i + 2];
-                i += 2;
+                score += 10 + rolls[frameIndex + 2];
+                frameIndex += 2;
             } else {
-                score += rolls[i] + rolls[i + 1];
-                i += 2;
+                score += rolls[frameIndex] +
+                        rolls[frameIndex + 1];
+                frameIndex += 2;
             }
         }
-
-
         return score;
+
     }
 
 }
