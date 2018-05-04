7c47ffa0afcbaeeaae444737676cf1cb98104aef
joancolmenerodev <joancolmenero@gmail.com>
2018-05-04 11:20:14 +0200

    Slide 23

--- src/test/java/com/joancolmenerodev/bownlingkata/BowlingGameTest.java
+++ src/test/java/com/joancolmenerodev/bownlingkata/BowlingGameTest.java
@@ -5,14 +5,27 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest extends TestCase {
+
+    private Game g;
+
+    protected void setUp() throws Exception {
+        g = new Game();
+    }
+
     public void testGutterGame() throws Exception {
         var g = new Game();
         for (var i=0; i<20; i++)
             g.roll(0);
         assertThat(g.score(),is(0));
-
-
     }
+
+    public void testAllOnes() throws Exception {
+        var g = new Game();
+        for (var i = 0; i < 20; i++)
+            g.roll(1);
+        assertThat(g.score(),is(20));
+    }
+
 
 }
 
