a5da29d41b80d430f9efb569f927d92692daccb6
joancolmenerodev <joancolmenero@gmail.com>
2018-05-04 11:29:29 +0200

    Slide 25

--- src/test/java/com/joancolmenerodev/bownlingkata/BowlingGameTest.java
+++ src/test/java/com/joancolmenerodev/bownlingkata/BowlingGameTest.java
@@ -15,9 +15,14 @@ public class BowlingGameTest extends TestCase {
     public void testGutterGame() throws Exception {
         var n = 20;
         var pins = 0;
-        for (var i=0; i<n; i++)
-            g.roll(pins);
+        rollMany(n, pins);
         assertThat(g.score(),is(0));
+    }
+
+    private void rollMany(int n, int pins) {
+        for(var i = 0; i < n; i++){
+            g.roll(pins);
+        }
     }
 
     public void testAllOnes() throws Exception {
