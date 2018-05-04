28618af98b2453aea2bf99c135ba5a80d2d3e684
joancolmenerodev <joancolmenero@gmail.com>
2018-05-04 11:38:23 +0200

    Slide 34

--- src/main/java/com/joancolmenerodev/bownlingkata/Game.java
+++ src/main/java/com/joancolmenerodev/bownlingkata/Game.java
@@ -2,9 +2,13 @@ package com.joancolmenerodev.bownlingkata;
 
 public class Game {
     private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
 
     public void roll(int pins) {
         score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
